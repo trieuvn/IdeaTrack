7381ff98e527662751d1b5c0927aab4f248ce993
trieuvn <trieu123ok@gmail.com>
2025-12-04 18:34:30 +0700

    init

---
+++ IdeaTrack/IdeaTrack/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IdeaTrack.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
