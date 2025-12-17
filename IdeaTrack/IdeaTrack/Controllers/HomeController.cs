737c7708b9df30e361c452eeef0a343253712a22
Nguyen Hoang Phuc <phucnguyen2716@gmail.com>
2025-12-17 12:40:16 +0700

    insert new view: history for author

--- IdeaTrack/IdeaTrack/Controllers/HomeController.cs
+++ IdeaTrack/IdeaTrack/Controllers/HomeController.cs
@@ -28,5 +28,9 @@ namespace IdeaTrack.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult History()
+        {
+            return View();
+        }
     }
 }
