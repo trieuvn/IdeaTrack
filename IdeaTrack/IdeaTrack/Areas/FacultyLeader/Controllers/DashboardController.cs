c11632bdd4ebba930d1bb6c85d58a760920d9dc7
Nguyen Hoang Phuc <phucnguyen2716@gmail.com>
2025-12-18 11:24:22 +0700

    Test areas successfully

---
+++ IdeaTrack/IdeaTrack/Areas/FacultyLeader/Controllers/DashboardController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdeaTrack.Areas.FacultyLeader.Controllers
+{
+    public class DashboardController : Controller
+    {
+        [Area("FacultyLeader")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- IdeaTrack/IdeaTrack/Program.cs
+++ IdeaTrack/IdeaTrack/Program.cs
@@ -46,7 +46,10 @@ app.UseRouting();
 
 app.UseAuthentication();  
 app.UseAuthorization();
-
+app.MapControllerRoute(
+    name: "areas",
+    pattern: "{area:exists}/{controller=Dashboard}/{action=Index}/{id?}"
+);
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
