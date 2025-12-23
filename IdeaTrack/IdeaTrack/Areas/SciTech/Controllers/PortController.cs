a0f9ef0a07f7411385109178f4388e141bf3ceed
phucnguyen2716 <phucnguyen2716@gmail.com>
2025-12-23 11:02:33 +0700

    Insert new View Port Science Technology

---
+++ IdeaTrack/IdeaTrack/Areas/SciTech/Controllers/PortController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdeaTrack.Areas.SciTech.Controllers
+{
+    [Area("SciTech")]
+    public class PortController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
