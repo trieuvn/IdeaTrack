cb234bc9d4948e780d96aec51d9b7d568d78828e
trieuvn <trieu123ok@gmail.com>
2026-01-14 13:11:21 +0700

    fix: search for category

--- IdeaTrack/IdeaTrack/Areas/Author/Controllers/InitiativeController.cs
+++ IdeaTrack/IdeaTrack/Areas/Author/Controllers/InitiativeController.cs
@@ -832,7 +832,7 @@ namespace IdeaTrack.Areas.Author.Controllers
 
         // ============ CATEGORY FILTER AJAX ENDPOINT ============
 
-        /*
+        
         // GET: /Author/Initiative/GetCategoriesByYear?yearId=xxx
         [HttpGet]
         public async Task<IActionResult> GetCategoriesByYear(int yearId)
@@ -868,7 +868,7 @@ namespace IdeaTrack.Areas.Author.Controllers
                 return Json(new List<object>());
             }
         }
-        */
+        
 
         // ============ CO-AUTHOR AJAX ENDPOINTS ============
 
